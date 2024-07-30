fc39e5879e3992845e0ed3b200df95b498109b4d
Claas <claas@claas.dev>
2024-07-30 21:44:51 +0200

    Refactor and set up for home assistant broker

--- mqtt/src/main.rs
+++ mqtt/src/main.rs
@@ -1,4 +1,5 @@
 mod variable_byte_integer;
+mod packet;
 
 use bytes::BytesMut;
 use std::net::{AddrParseError, SocketAddr, ToSocketAddrs};
@@ -27,23 +28,14 @@ enum AppError {
     DotenvError(#[from] dotenvy::Error),
     #[error("Error reading environment variable")]
     EnvVarError(#[from] env::VarError),
-}
 
-async fn set_up_tcp_connection(
-    broker_address: String,
-) -> Result<impl AsyncReadExt + AsyncWriteExt, AppError> {
-    let address = (broker_address.as_ref(), 8883)
-        .to_socket_addrs()?
-        .next()
-        .unwrap();
-
-    let stream = TcpStream::connect(&address).await?;
-
-    Ok(stream)
+    #[error("Port environment variable is not a valid number")]
+    PortEnvVarError(#[from] std::num::ParseIntError),
 }
 
 async fn set_up_tls_connection(
     broker_address: String,
+    port: u16,
 ) -> Result<impl AsyncReadExt + AsyncWriteExt, AppError> {
     let mut root_certificate_store = RootCertStore::empty();
     root_certificate_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
@@ -53,11 +45,11 @@ async fn set_up_tls_connection(
         .with_no_client_auth();
     let connector = TlsConnector::from(Arc::new(configuration));
 
-    let address = (broker_address.as_ref(), 8883)
+    let address = (broker_address.as_ref(), port)
         .to_socket_addrs()?
         .next()
         .unwrap();
-    // let address = SocketAddr::from_str(MQTT_BROKER_ADDRESS)?;
+
     let server_name = ServerName::try_from(broker_address)?;
 
     let stream = TcpStream::connect(&address).await?;
@@ -67,210 +59,18 @@ async fn set_up_tls_connection(
     Ok(stream)
 }
 
-fn create_connect_packet(client_identifier: &str, username: &str, password: &[u8]) -> Vec<u8> {
-    //TODO validate identifier length is between 1 and 23 bytes and contains only valid characters
-    let identifier_length = client_identifier.len() as u16;
+async fn set_up_tcp_connection(
+    broker_address: String,
+    broker_port: u16,
+) -> Result<impl AsyncReadExt + AsyncWriteExt, AppError> {
+    let address = (broker_address.as_ref(), broker_port)
+        .to_socket_addrs()?
+        .next()
+        .unwrap();
 
-    // Length of str might not be the same as length of bytes afaik
-    let username = username.as_bytes();
-    //TODO validate user name is less than u16::MAX
-    let username_length = username.len() as u16;
-    //TODO validate password is less than u16::MAX
-    let password_length = password.len() as u16;
+    let stream = TcpStream::connect(&address).await?;
 
-    // Remaining length can only be a byte
-    //TODO check length is not exceeding u8
-
-    // 2 = length of length fields
-    let remaining_length: u8 = (VARIABLE_HEADER.len() as u16
-        + 2
-        + identifier_length
-        + 2
-        + username_length
-        + 2
-        + password_length) as u8;
-
-    // Fixed header
-    let fixed_header = [
-        // CONNECT package type (1) in the first four bits
-        1 << 4,
-        // Remaining length
-        remaining_length,
-    ];
-
-    // Create variable header
-    const VARIABLE_HEADER: [u8; 11] = [
-        // Protocol name length
-        0x00,
-        0x04,
-        // Protocol name
-        b'M',
-        b'Q',
-        b'T',
-        b'T',
-        // Protocol version
-        5,
-        // Connect Flags
-        // USER_NAME_FLAG | PASSWORD_FLAG | CLEAN_START
-        0b1100_0010,
-        // Keep alive = 60 seconds
-        0x00,
-        60,
-        // Property length 0 (no properties). Has to be set to 0 if there are no properties
-        0,
-    ];
-
-    let mut packet = Vec::with_capacity(fixed_header.len() + remaining_length as usize);
-    // packet.append(fixed_header)
-    packet.extend_from_slice(&fixed_header);
-
-    // Variable header
-    packet.extend_from_slice(&VARIABLE_HEADER);
-
-    // Payload
-
-    // Client identifier
-    let length_bytes = identifier_length.to_be_bytes();
-    // Client identifier length
-    packet.push(length_bytes[0]);
-    packet.push(length_bytes[1]);
-    // Client identifier
-    packet.extend_from_slice(client_identifier.as_bytes());
-
-    // User name
-    let length_bytes = username_length.to_be_bytes();
-    packet.push(length_bytes[0]);
-    packet.push(length_bytes[1]);
-
-    // User name
-    packet.extend_from_slice(username);
-
-    // Password is any binary data
-    let length_bytes = password_length.to_be_bytes();
-    packet.push(length_bytes[0]);
-    packet.push(length_bytes[1]);
-    // Password
-    packet.extend_from_slice(password);
-
-    packet
-}
-
-fn create_publish_packet(topic_name: &str, payload: &[u8]) -> Vec<u8> {
-    let mut packet = Vec::with_capacity(0);
-    // Shadowing topic name to not confuse str and bytes length
-    let topic_name = topic_name.as_bytes();
-    let topic_name_length = topic_name.len() as u16;
-    let payload_length = payload.len() as u16;
-
-    // Fixed header
-    // Packet type PUBLISH (3) and flags
-    packet.push(3 << 4);
-    //TODO ensure remaining length is less than max u8
-
-    // 2 = name length
-    // 1 = length property length bytes
-    let remaining_length: u8 = (2 + topic_name_length + 1 + payload_length) as u8;
-    packet.push(remaining_length);
-
-    // Variable header
-    // Topic name length
-    let length_bytes = topic_name_length.to_be_bytes();
-    packet.push(length_bytes[0]);
-    packet.push(length_bytes[1]);
-
-    // Topic name
-    packet.extend_from_slice(topic_name);
-
-    // Property length
-    // No properties supported for now so set to 0
-    packet.push(0);
-
-    // Payload
-    // No need to set length as it can be calculated
-    packet.extend_from_slice(payload);
-
-    packet
-}
-
-#[test]
-fn can_create_publish_packet() {
-    const PAYLOAD: [u8; 11] = *b"testpayload";
-    // Act
-    let publish_packet = create_publish_packet("test", &PAYLOAD);
-
-    // Assert
-    // Fixed header
-    assert_eq!(publish_packet[..2], [0b0011_0000, 18]);
-
-    // Variable header
-    assert_eq!(
-        publish_packet[2..9],
-        [
-            // Topic name length
-            0x00, 0x04, // Topic name
-            b't', b'e', b's', b't', // Property length
-            0x00,
-        ]
-    );
-
-    // Payload
-    assert_eq!(publish_packet[9..], PAYLOAD);
-}
-
-#[test]
-fn can_create_connect_packet() {
-    // Act
-    let connect_packet = create_connect_packet("mqttx_0x668d0d", "admin", b"public");
-    // Assert
-    // Fixed header
-    assert_eq!(connect_packet[..2], [0b0001_0000, 42]);
-    // Variable header
-    assert_eq!(
-        connect_packet[2..13],
-        [
-            // Protocol name length
-            0x00,
-            0x04,
-            // Protocol name
-            b'M',
-            b'Q',
-            b'T',
-            b'T',
-            // 0x4d, 0x51, 0x54, 0x54,
-            // Protocol version
-            5,
-            // Connect Flags
-            // USER_NAME_FLAG | PASSWORD_FLAG | CLEAN_START
-            0b1100_0010,
-            // 0xc2,
-            // Keep alive 60 seconds
-            0x00,
-            60,
-            // Property length 0 (no properties). Has to be set to 0 if there are no properties
-            0,
-        ]
-    );
-
-    // Payload
-    assert_eq!(
-        connect_packet[13..],
-        // Rust formatting is doing some wild stuff here for some reason
-        #[rustfmt::skip]
-        [
-            // Client identifier length
-            0x00, 0x0e,
-            // Client identifier
-            b'm', b'q', b't', b't', b'x', b'_', b'0', b'x', b'6', b'6', b'8', b'd', b'0', b'd',
-            // User name length
-            0x00, 0x05,
-            // User name
-            0x61, 0x64, 0x6d, 0x69, 0x6e,
-            // Password length
-            0x00, 0x06,
-            // Password
-            b'p', b'u', b'b', b'l', b'i', b'c',
-        ]
-    );
+    Ok(stream)
 }
 
 #[tokio::main]
@@ -278,17 +78,19 @@ async fn main() -> Result<(), AppError> {
     // Set up .env environment variables
     dotenvy::dotenv()?;
     let broker_address = env::var("MQTT_BROKER_ADDRESS")?;
+    let broker_port: u16 = env::var("MQTT_BROKER_PORT")?.parse()?;
     let username = env::var("MQTT_BROKER_USERNAME")?;
+    // The password might need to be surrounded by single quotes (e.g. 'password') to be read correctly
     let password = env::var("MQTT_BROKER_PASSWORD")?;
 
-    let mut stream = set_up_tls_connection(broker_address).await?;
+    let mut stream = set_up_tcp_connection(broker_address, broker_port).await?;
 
     // MQTT
     // Good resource: https://www.emqx.com/en/blog/mqtt-5-0-control-packets-01-connect-connack
     // 3.1 CONNECT - Connection request
 
     let connect_packet =
-        create_connect_packet("testclient", username.as_str(), password.as_bytes());
+        packet::create_connect("testclient", username.as_str(), password.as_bytes());
 
     stream.write_all(&connect_packet).await?;
 
@@ -363,7 +165,7 @@ async fn main() -> Result<(), AppError> {
 
     println!("Sending PUBLISH");
     // Publish a message
-    let publish_packet = create_publish_packet("test", b"testpayload");
+    let publish_packet = packet::create_publish("test", b"testpayload");
     stream.write_all(&publish_packet).await?;
     // stream
     //     .write_all(&[
