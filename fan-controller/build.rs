4182f90c9e1f59048de73c31c120e81b25ca7391
Claas <claas@claas.dev>
2025-07-07 21:48:01 +0200

    Make secret injection process less of a hassle

--- fan-controller/build.rs
+++ fan-controller/build.rs
@@ -8,19 +8,30 @@
 //! updating `memory.x` ensures a rebuild of the application with the
 //! new memory settings.
 
-use std::env;
+use std::env::{self, VarError};
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 
-fn main() {
+#[derive(Debug, thiserror::Error)]
+enum BuildError {
+    #[error("Failed to write memory.x file: {0}")]
+    WriteMemoryXFile(#[from] std::io::Error),
+
+    #[error("Failed to load .env file for Wifi credentials: {0}")]
+    LoadEnvFile(#[from] dotenvy::Error),
+
+    #[error("Missing variable {1} in .env file: {0}")]
+    MissingEnvVar(VarError, &'static str),
+}
+
+fn ensure_memory_x_file() -> Result<(), BuildError> {
     // Put `memory.x` in our output directory and ensure it's
     // on the linker search path.
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
     File::create(out.join("memory.x"))
         .unwrap()
-        .write_all(include_bytes!("memory.x"))
-        .unwrap();
+        .write_all(include_bytes!("memory.x"))?;
     println!("cargo:rustc-link-search={}", out.display());
 
     // By default, Cargo will re-run a build script whenever
@@ -33,4 +44,28 @@ fn main() {
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tlink-rp.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
+
+    Ok(())
+}
+
+fn setup_wifi_credentials() -> Result<(), BuildError> {
+    const WIFI_NETWORK: &str = "FAN_CONTROL_WIFI_NETWORK";
+    const WIFI_PASSWORD: &str = "FAN_CONTROL_WIFI_PASSWORD";
+
+    dotenvy::dotenv()?;
+    let wifi_network =
+        env::var(WIFI_NETWORK).map_err(|error| BuildError::MissingEnvVar(error, WIFI_NETWORK))?;
+    let wifi_password =
+        env::var(WIFI_PASSWORD).map_err(|error| BuildError::MissingEnvVar(error, WIFI_PASSWORD))?;
+
+    println!("cargo:rustc-env=FAN_CONTROL_WIFI_NETWORK={wifi_network}");
+    println!("cargo:rustc-env=FAN_CONTROL_WIFI_PASSWORD={wifi_password}");
+
+    Ok(())
+}
+
+fn main() -> Result<(), BuildError> {
+    ensure_memory_x_file()?;
+    setup_wifi_credentials()?;
+    Ok(())
 }
